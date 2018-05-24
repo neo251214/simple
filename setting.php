919c132198eed7b99e113aee80c7048109567020
neo251214 <36342681+neo251214@users.noreply.github.com>
2018-05-24 09:55:09 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -2,11 +2,11 @@
 
 class Setting {
 	public function getChannelAccessToken(){
-		$channelAccessToken = "";
+		$channelAccessToken = "J6A2zs5tta+NL/+gwyGoYnNTX2XN0qXeU7AMDMcDvtyg8D5hT6KtiS7sPT++cI5/7YN1I3vWVLaaz2H3QK82/b+QJllx6sOKp5Nsj6IGz8XjfeZX9uSHDKUIdo90+U7QxrNMegBcrNth88NTdl21qQdB04t89/1O/w1cDnyilFU=";
 		return $channelAccessToken;
 	}
 	public function getChannelSecret(){
-		$channelSecret = "";
+		$channelSecret = "a43e7333ad8bba1e05bbf01707a97d20";
 		return $channelSecret;
 	}
 	public function getApiReply(){
